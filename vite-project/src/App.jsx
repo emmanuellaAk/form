fe32df1487fde91179df25423f765e03873abd3f
Emmanuella Animwaa Kumah <emmanuellaanimwaa17@icloud.com>
2024-01-03 00:57:32 +0000

    tring to connect

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,6 +1,32 @@
 import React from "react";
 
+let itemList = [];
+
+function addItem(item) {
+  if (!itemList.includes(item)) {
+    itemList.push(item);
+    console.log(`${item} added successfully.`);
+  } else {
+    console.log(`${item} is already in the list.`);
+  }
+}
+addItem();
 function App() {
+  <button id="addButton">Add Task</button>;
+
+  fetch("localhost:8000", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(Todo - List),
+  })
+    .then((response) => response.json())
+    .then((data) => console.log(data))
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+
   return (
     <>
       <div class="container">
