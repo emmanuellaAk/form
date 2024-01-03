cbf45992090de3b181591538912a134a766bd675
Emmanuella Animwaa Kumah <emmanuellaanimwaa17@icloud.com>
2024-01-03 00:13:44 +0000

    still connecting

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const mongoose = require('mongoose')
 const Todo = require('./models/todo')
 const path = require('path');
 
-app.use(express.static(path.join(__dirname, '../vite-project/dist')));
+app.use(express.static(path.join(__dirname, 'vite-project')));
+// didn't include the ../ part because the static middleware uses an absolute path. it u don't need to step out with ../
 
 app.use(express.json())
 
@@ -21,7 +22,8 @@ app.post('/List',async(req,res)=>{
 app.get('/List',async(req,res) => {
   try{
      const todo = await Todo.find({});
-     res.status(200).json (todo);
+     res.status(200).json(todo);
+     res.send("Hello");
   }  catch(error) {
      res.status(500).json({message:error.message})
   }
@@ -67,17 +69,10 @@ app.delete('/List/:id',async(req,res) => {
   }
 })
 
-
-
-
-
-
-
-
-
-
-
-app.listen(8000, () => {
-  console.log("Server is running on port 8000");
-});
-
+mongoose.connect('mongodb://localhost:27017')
+.then(()=>{
+    app.listen(8000, () => {
+        console.log("Server is running on port 8000");
+      });      
+})
+//the connect fxn is async meaning it takes sometime to do, i attached a then block because i want to start the server only when i am connected to the server
